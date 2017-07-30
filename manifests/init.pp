11b1c662d63793c48830e78e7f015c5ee79bd09d
root <root@main.reviakin.net>
2017-07-30 15:11:02 +0500

    initial

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+class sox {
+	$tmpdir='/tmp/sox_inst'
+	include lame
+	file {$tmpdir:
+		ensure=>directory
+	} ->
+	file {"$tmpdir/sox.tar.gz":
+		ensure	=> file,
+		source	=> 'puppet:///modules/sox/sox-14.4.1.tar.gz',
+	} ->
+	exec {'install_sox':
+		command	=> 'tar -zxvf ./sox.tar.gz && cd sox* && ./configure && make && make install',
+		cwd		=> $tmpdir,
+		unless	=> 'which sox && sox --help-format mp3|grep "Format: mp3"',
+		path	=> '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin',
+		require => [
+			Package['kernel-devel'],
+			Exec['install_lame']
+		]
+	}
+}
+
