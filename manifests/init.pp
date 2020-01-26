e8e7b9deaa43400de295d2b1539b8f3b387b14f9
Alexander Reviakin <reviakin@gmail.com>
2020-01-26 22:43:31 +0500

    build string fixed

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@ class sox {
 		source	=> 'puppet:///modules/sox/sox-14.4.1.tar.gz',
 	} ->
 	exec {'install_sox':
-		command	=> 'tar -zxvf ./sox.tar.gz && cd sox* && ./configure && make && make install',
+		command	=> 'tar -zxvf ./sox.tar.gz && cd sox-14.4.1 && ./configure && make && make install',
 		cwd		=> $tmpdir,
 		unless	=> 'which sox && sox --help-format mp3|grep "Format: mp3"',
 		path	=> '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin',
